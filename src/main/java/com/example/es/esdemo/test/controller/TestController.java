b299588e5f50cf8aa04d4c098348919c5aa3b8b6
李小龙 <lixiaolong@gooagoo.com>
2019-10-23 15:24:31 +0800

    add query dsl

--- src/main/java/com/example/es/esdemo/test/controller/TestController.java
+++ src/main/java/com/example/es/esdemo/test/controller/TestController.java
@@ -1,9 +1,14 @@
 package com.example.es.esdemo.test.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.example.es.esdemo.test.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @RestController
 public class TestController {
@@ -16,4 +21,14 @@ public class TestController {
         testService.bulkSave();
         return "success";
     }
+
+
+    @RequestMapping("/queryDSL")
+    public String queryDSL(HttpServletRequest request) {
+        String json = request.getParameter("json");
+        Map param = JSON.parseObject(json, Map.class);
+        testService.queryDSL(param);
+        return "";
+    }
+
 }
--- src/main/java/com/example/es/esdemo/test/service/TestService.java
+++ src/main/java/com/example/es/esdemo/test/service/TestService.java
@@ -4,11 +4,22 @@ package com.example.es.esdemo.test.service;
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.TermsQueryBuilder;
+import org.elasticsearch.index.query.WildcardQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -21,7 +32,7 @@ public class TestService {
     private JdbcTemplate jdbcTemplate;
 
     public String bulkSave() {
-        int pageSize = 5000;
+        int pageSize = 10000;
         int pageTotal = 50;
         for (int i = 0; i < pageTotal; i++) {
             int start = i * pageSize;
@@ -32,15 +43,18 @@ public class TestService {
         return "success";
     }
 
+    /**
+     * bulk insert into  es
+     *
+     * @param maps
+     */
     private void doBulk(List<Map<String, Object>> maps) {
         if (maps.isEmpty()) {
             return;
         }
         BulkRequestBuilder bulkRequest = client.prepareBulk();
         maps.forEach(map -> {
-            String number = (String) map.get("number");
-            double saleNumber = convert2Double(number);
-            map.put("sale_product_number", saleNumber);
+            handleData(map);
             IndexRequestBuilder indexBuilder = client.prepareIndex("gag_i_supervise_pharmacy_out", "pharmacy").setSource(map);
             bulkRequest.add(indexBuilder);
         });
@@ -51,10 +65,104 @@ public class TestService {
         }
     }
 
+    void handleData(Map data) {
+        String number = (String) data.get("number");
+        double saleNumber = convert2Double(number);
+        data.put("sale_product_number", saleNumber);
+        String issuanceDate = (String) data.get("issuance_date");
+        String issueDateCollated = convertStr2Date(issuanceDate);
+        if (issueDateCollated == null) {
+            System.err.println(" convert failed :  issuance_date  for value  " + issuanceDate);
+        } else {
+            data.put("issuance_date_collate", issueDateCollated);
+        }
+        String validityTime = (String) data.get("validity_time");
+        String validityTimeCollated = convertStr2Date(validityTime);
+        if (validityTimeCollated == null) {
+            System.err.println(" convert failed :  validity_time  for value  " + validityTime);
+        } else {
+            data.put("validity_time_collate", validityTimeCollated);
+        }
+    }
 
+
+    /**
+     * @param dateStr
+     * @return
+     */
+    String convertStr2Date(String dateStr) {
+        if ("".equals(dateStr) || "-".equals(dateStr)) {
+            return null;
+        }
+        try {
+            String pattern = "yyyy-MM-dd";
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+            LocalDate localDate = LocalDate.parse(dateStr, formatter);
+            return localDate.format(formatter);
+        } catch (Exception e) {
+        }
+        try {
+            String pattern1 = "yyyy-MM-dd HH:mm:ss";
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern1);
+            LocalDateTime localDateTime = LocalDateTime.parse(dateStr, formatter);
+            return localDateTime.format(formatter);
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+
+    /**
+     * convert string to double
+     *
+     * @param data
+     * @return
+     */
     double convert2Double(String data) {
         return Double.valueOf(data).doubleValue();
     }
 
+
+    public void queryDSL(Map param) {
+        String productNameQuery = (String) param.get("productName");
+        Integer start = Integer.valueOf((String) param.get("start"));
+        Integer pageSize = Integer.valueOf((String) param.get("size"));
+        BoolQueryBuilder builder = new BoolQueryBuilder();
+        WildcardQueryBuilder productName = new WildcardQueryBuilder("product_name.raw", productNameQuery);
+        builder.filter().add(productName);
+        TermsQueryBuilder termsQueryBuilder = new TermsQueryBuilder("batch_number", new String[]{"无", "******", "-"});
+        builder.mustNot().add(termsQueryBuilder);
+        String query = builder.toString();
+        System.err.println(query);
+        SearchResponse sr = client.prepareSearch("gag_i_supervise_pharmacy_out")
+                .setQuery(builder)
+                .addSort("", SortOrder.DESC)
+                .setFrom(start)
+                .setSize(pageSize)
+                .get();
+        SearchHits hits = sr.getHits();
+        long totalHits = hits.getTotalHits();
+        System.err.println("totals ：" + totalHits);
+        Iterator<SearchHit> iterator = hits.iterator();
+        while (iterator.hasNext()) {
+            SearchHit next = iterator.next();
+            String nodeId = next.getShard().getNodeId();
+            int id = next.getShard().getShardId().id();
+            Map<String, Object> source = next.getSource();
+            System.err.println("nodeId : " + nodeId + " shardId : " + id + " data : " + source);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        TestService testService = new TestService();
+        System.err.println(testService.convertStr2Date("2018-01-01"));
+        System.err.println(testService.convertStr2Date("2018-01-01 12:20:30"));
+        System.err.println(testService.convertStr2Date("2018-01-01-12:20:30"));
+        System.err.println(testService.convertStr2Date(""));
+        System.err.println(testService.convertStr2Date("-"));
+        System.err.println(testService.convertStr2Date("+"));
+    }
 
 }
